3a5fa104ccb5f0f43f56bcff902855e62d0c8bc9
Dan Kelly <danielkelly89@googlemail.com>
2018-09-27 13:46:37 +0200

    Script now writes to file itself instead of sending stdout to file

--- get_ip_set.py
+++ get_ip_set.py
@@ -5,18 +5,23 @@ import boto3
 def getipset(ipsetid):
     client = boto3.client('waf')
     response = client.get_ip_set (
-        IPSetId=ipsetid, 
+        IPSetId=ipsetid,
     )
-    
+
     output = ""
-    
+
     for key in response["IPSet"]["IPSetDescriptors"]:
        for value in key.iteritems():
           if value[1] != "IPV4":
              output += 'ip_set_descriptors {\n\ttype = "IPV4"\n\tvalue = "' + value[1] + '"\n}\n\n'
-    
+
     return output
+
+def writetofile(data):
+    f = open("ipset.tf", "w")
+    f.write(data)
+    return "IP's written to file ipset.tf"
 
 if __name__ == "__main__":
     ipsetid = raw_input("Enter IP Set ID: ")
-    print(getipset(ipsetid))
+    print(writetofile(getipset(ipsetid)))
